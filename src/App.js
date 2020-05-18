4ee8e419fbbbed9dc7eeb90e8b77f21ec6927f28
Damir Mehic <damir.mehic@legal.one>
2020-05-18 07:49:49 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there! Damir! Sta je ovo jel se menja!
         </p>
         <a
           className="App-link"
